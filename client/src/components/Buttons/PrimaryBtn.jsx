3463e45145d13e3f4a38f60c437eaa4a81afdeb6
gajj44 <gajjunaphade@gmail.com>
2025-01-03 00:48:35 +0530

    fixed navbar responsive, added home hero section

---
+++ client/src/components/Buttons/PrimaryBtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { memo } from 'react'
+
+const PrimaryBtn= memo(({children,className}) => {
+  return (
+   <button className={`p-4 flex py-3 border-2 border-[#725B98] rounded-[5px] font-Inter font-bold bg-[#725B98] ${className} text-white hover:bg-transparent hover:text-[#725B98] gap-2 transition-all duration-500 `}>
+    {children}
+   </button>
+  )
+});
+
+export default PrimaryBtn
