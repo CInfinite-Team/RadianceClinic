13092e9433506db1fb74d7168fad0510aae7d5c5
gajju44 <gajjunaphade@gmail.com>
2025-01-29 22:08:41 +0530

    build fix

--- client/src/pages/HairForm.jsx
+++ client/src/pages/HairForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import PatientFormsTitle from '../components/SharedComponents/PatientFormsTitle'
-import HairformComponent from '../components/HairForm/HairformComponent'
+import HairformComponent from '../components/HairForm/HairFormComponent.jsx'
 import Footer from '../components/footer/Footer'
 
 function HairForm() {
