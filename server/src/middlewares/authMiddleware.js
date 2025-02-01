10c882be8c230c0b5cc8aac92e472eed50a0d0a3
ankitkatole <katoleankit06@gmail.com>
2025-02-01 22:53:18 +0530

    Updated Middlesware

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../../constants');
 
 const authenticateAdmin = (req, res, next) => {
-    const token = req.header('Authorization');
+    const token = req.header('Authorization')?.split(' ')[1];
     if (!token) {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
