7b0dab3bbf5ccf2c416566ad1e1a01cd3bf00e4b
ankitkatole <katoleankit06@gmail.com>
2025-02-01 22:09:42 +0530

    CORS

--- server/server.js
+++ server/server.js
@@ -4,27 +4,30 @@ const cors = require('cors');
 
 const { PORT } = require('./constants');
 const formRouter = require('./src/routes/forms');
-const adminRouter = require('./src/routes/adminRoutes'); 
+const adminRouter = require('./src/routes/adminRoutes');
 const userRouter = require('./src/routes/userRoutes');
 
 const connectDB = require('./src/db/connection');
 
 if (require.main === module) {
     const app = express();
-    app.use(express.json());
 
-    // Enable CORS
+    // CORS options to allow all origins (or you can specify your frontend URL)
     const corsOptions = {
-        origin: true,  // Allow requests from any origin
+        origin: '*', // Allow all origins
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
     };
+
+    // Enable CORS
+    app.use(cors(corsOptions)); // Apply CORS to all routes
+    app.options('*', cors(corsOptions)); // Handle preflight requests
 
     // Routes
     app.use('/api/forms', formRouter); // User routes
     app.use('/api/admin', adminRouter); // Admin routes
     app.use('/api/user', userRouter); // User routes
-  
+ 
     app.listen(PORT, () => {
         console.log(`Server running at http://localhost:${PORT}/`);
         connectDB();
