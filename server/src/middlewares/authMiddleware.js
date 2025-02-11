1913fe6562f0ad0dd0e571a7201aa93e2fa21682
ankitkatole <katoleankit06@gmail.com>
2025-02-11 21:49:57 +0530

    auth

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -3,13 +3,13 @@ const { SECRET_KEY } = require('../../constants');
 
 const authenticateAdmin = (req, res, next) => {
     const authHeader = req.header('Authorization');
-    
+    console.log('authHeader:', authHeader);
     if (!authHeader) {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
 
     const token = authHeader.startsWith('Bearer ') ? authHeader.split(' ')[1] : authHeader;
-
+    console.log('token:', token);
     if (!token) {
         return res.status(401).json({ message: 'Access denied. No token provided.' });
     }
