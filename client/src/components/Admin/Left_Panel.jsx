7a929a195e7e746e24d2008e749286191c5d4147
gajju44 <gajjunaphade@gmail.com>
2025-02-11 12:31:28 +0530

    optimized left panel for mobile

--- client/src/components/Admin/Left_Panel.jsx
+++ client/src/components/Admin/Left_Panel.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import {useState} from 'react'
 import PrimaryBtn from '../Buttons/PrimaryBtn';
 import Logo from '../../assets/SharedAssets/adminPanel_logo.png';
 import dashboard from '../../assets/Admin/dashboard.png';
 import leads from '../../assets/Admin/leads.png';
 import appointments from '../../assets/Admin/appointments.png';
 import forms from '../../assets/Admin/forms.png';
+import { Menu,X } from 'lucide-react';
 
 function Left_Panel({handleLinkClick,selectedLink}) {
+
+  const [ShowNavbar,setShowNavbar]=useState(false);
+
   return (
    <>
-   <div className="flex-[1] bg-white h-screen p-6 flex flex-col items-center ">
+    <button onClick={()=> setShowNavbar(!ShowNavbar)} className='fixed left-4 top-4 z-[80] p-2 bg-[#0e0e0e93] transition-all duration-500 backdrop-blur-sm rounded-full text-purple-200 border '>
+   { !ShowNavbar? <Menu size={32}/>
+          : <X size={32}/>}
+        </button>
+   <div className={`flex-1 bg-white h-screen ${ShowNavbar === true ? 'translate-x-0' : '-translate-x-full' } transition-all duration-500 md:translate-x-0 p-6 flex flex-col fixed border-r-2 border-[#3A2E2B] border-opacity-[20%] md:static z-50 items-center `}>
+        
+       
+
         <div className="text-center mb-6 pb-6">
           <img src={Logo} alt="Logo" className="h-auto w-auto" />
         </div>
@@ -68,11 +79,11 @@ function Left_Panel({handleLinkClick,selectedLink}) {
 
         <div className="mt-auto">
           <PrimaryBtn
-             className={'px-20 py-2 rounded-md xs:px-2 sm:px-6 md:px-20 xs:text-xs md:text-lg'}
+             className={'px-20 py-2 rounded-md  whitespace-nowrap  xs:text-xs md:text-lg'}
         >Log out</PrimaryBtn>
         </div>
       </div>
-       <div className="w-[2px] bg-gray-300"></div>
+       
    </>
   )
 }
