9074610ef15563065b26fc84c3a2b2baf7e7b55d
ankitkatole <katoleankit06@gmail.com>
2025-02-14 13:19:16 +0530

    aa

--- server/src/controllers/blogController.js
+++ server/src/controllers/blogController.js
@@ -16,7 +16,6 @@ const createBlog = async (req, res) => {
 
         if (req.file) {
             const fileExt = path.extname(req.file.originalname).toLowerCase();
-
             if (fileExt === '.jpg' || fileExt === '.jpeg') {
                 imageType = 'image/jpeg';
             } else if (fileExt === '.png') {
@@ -24,16 +23,20 @@ const createBlog = async (req, res) => {
             } else {
                 return res.status(400).json({ message: 'Invalid file format! Only JPG, JPEG, and PNG are allowed.' });
             }
-
             imageData = req.file.buffer;
         }
 
+        // Parse the content if it's a string
+        let parsedContent = content;
+        if (typeof content === 'string') {
+            parsedContent = JSON.parse(content);
+        }
         const categoryLower = category.toLowerCase();
 
         const newBlog = new Blog({
             title,
             introduction,
-            content,
+            content: parsedContent,  // Use parsed array of objects
             category: categoryLower,
             image: imageData ? { data: imageData, contentType: imageType } : undefined,
             admin: {
