b67a94351407ef989f679ba80d27651723e22130
gajju44 <gajjunaphade@gmail.com>
2025-02-15 17:30:08 +0530

    changed about us testimonial

---
+++ client/src/components/AboutUS/TestimonialSlide.jsx
@@ -0,0 +1,65 @@
+import React, { useRef, useState } from "react";
+import { Play, Pause } from "lucide-react";
+import InvertedComa from "../../assets/SharedAssets/InvertedComa.svg";
+
+const TestimonialSlide = ({ data }) => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleVideoClick = () => {
+    if (isPlaying) {
+      videoRef.current.pause();
+      setIsPlaying(false);
+    } else {
+      videoRef.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <div className="flex flex-col gap-10  justify-center mb-10 items-center">
+      {" "}
+      <div className="w-[180px] h-[180px] md:w-[200px] md:h-[200px] lg:w-[260px] lg:h-[260px] relative aspect-square rounded-full">
+        {" "}
+        <div className="z-10 overflow-hidden rounded-full w-full h-full bg-black relative">
+          {" "}
+          <video
+            ref={videoRef}
+            src={data.video}
+            className="w-full h-full"
+            // muted
+            playsInline
+            onEnded={() => setIsPlaying(false)}
+          />{" "}
+          <button
+            onClick={handleVideoClick}
+            className="bg-[#ffffff86] p-2 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition duration-500 hover:scale-110"
+          >
+            {" "}
+            <span className="bg-[#ffffff94] rounded-full w-12 h-12 flex items-center justify-center">
+              {" "}
+              {isPlaying ? (
+                <Pause color="white" />
+              ) : (
+                <Play color="white" />
+              )}{" "}
+            </span>{" "}
+          </button>{" "}
+        </div>{" "}
+        <div className="p-2 md:p-3 z-10 lg:p-4 rounded-full bg-white absolute right-0 bottom-0">
+          {" "}
+          <img src={InvertedComa} alt="Inverted Coma" className="w-8" />{" "}
+        </div>{" "}
+      </div>{" "}
+      <div className="flex flex-col">
+        <h3 className="text-4xl text-[#CDAE62] font-ElMessiri">{data.Name}</h3>
+        <p className="text-[#554075] text-center">{data.Position}</p>
+      </div>
+      <p className="text-[#554075] z-10 text-center w-[90vw] lg:w-[76vw] xl:w-[54vw] 2xl:w-[51%]">
+        ‘‘{data.Review}’’
+      </p>
+    </div>
+  );
+};
+
+export default TestimonialSlide;
--- client/src/components/SharedComponents/Testimonial.jsx
+++ client/src/components/SharedComponents/Testimonial.jsx
@@ -1,121 +1,73 @@
-import React from 'react'
-import testimonial from '../../assets/SharedAssets/testimonial.svg'
-import InvertedComa from '../../assets/SharedAssets/InvertedComa.svg'
+import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import WhitePlant from '../../assets/Aboutus/WhitePlant.svg'
-import Leaf from '../../assets/SharedAssets/Leaf.svg'
-import Whiteobject from '../../assets/SharedAssets/Whiteobject.svg'
-import Yogesh_review from '../../assets/Home/Review_Hairs/Yogesh_review.mp4'
-import Mayur_Karro_Review from '../../assets/Home/Review_Hairs/Mayur_Karro_Review.mp4'
-import Mustafa from '../../assets/Home/Review_Hairs/Mustafa.mp4'
+import TestimonialSlide from '../AboutUS/TestimonialSlide';
+import WhitePlant from '../../assets/Aboutus/WhitePlant.svg';
+import Leaf from '../../assets/SharedAssets/Leaf.svg';
+import Whiteobject from '../../assets/SharedAssets/Whiteobject.svg';
+import Yogesh_review from '../../assets/Home/Review_Hairs/Yogesh_review.mp4';
+import Mayur_Karro_Review from '../../assets/Home/Review_Hairs/Mayur_Karro_Review.mp4';
+import Mustafa from '../../assets/Home/Review_Hairs/Mustafa.mp4';
 
 const sliderSettings = {
   dots: true,
   infinite: true,
   speed: 1000,
-    fade: false,
+  fade: false,
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
   autoplay: true,
-
 };
 
-const TestimonialData =[
+const TestimonialData = [
   {
-    video:Yogesh_review ,
-    Treatment:'Hair Transplant',
-    Name:'Yogesh',
-    Position:'Lawyer',
-    Review:'Barde Radiance really the best ever in India mostly Face-lift service  is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly  Face-lift',
-},
-{
-    video:Mayur_Karro_Review,
-    Treatment:'Scalp Micropigmentation',
-    Name:'Mayur Karro',
-    Position:'Lawyer',
-    Review:'Barde Radiance really the best ever in India mostly Face-lift service  is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly  Face-lift',
-},
-{
-    video:Mustafa,
-    Treatment:'PRP Therapy',
-    Name:'Mustafa',
-    Position:'Lawyer',
-    Review:'Barde Radiance really the best ever in India mostly Face-lift service  is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly  Face-lift',
-},
-  // {
-  //   Name:'Deepika Agarwal',
-  //   Position:'Lawyer',
-  //   Review:'Barde Radiance really the best ever in India mostly Face-lift service  is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly  Face-lift',
-  // },
-  // {
-  //   Name:'Deepika Agarwal',
-  //   Position:'Lawyer',
-  //   Review:'Barde Radiance really the best ever in India mostly Face-lift service  is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly  Face-lift',
-  // },
-  // {
-  //   Name:'Deepika Agarwal',
-  //   Position:'Lawyer',
-  //   Review:'Barde Radiance really the best ever in India mostly Face-lift service  is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly  Face-lift',
-  // },
- 
-]
+    video: Yogesh_review,
+    Treatment: 'Hair Transplant',
+    Name: 'Yogesh',
+    Position: 'Lawyer',
+    Review: 'Barde Radiance really the best ever in India mostly Face-lift service is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly Face-lift',
+  },
+  {
+    video: Mayur_Karro_Review,
+    Treatment: 'Scalp Micropigmentation',
+    Name: 'Mayur Karro',
+    Position: 'Lawyer',
+    Review: 'Barde Radiance really the best ever in India mostly Face-lift service is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly Face-lift',
+  },
+  {
+    video: Mustafa,
+    Treatment: 'PRP Therapy',
+    Name: 'Mustafa',
+    Position: 'Lawyer',
+    Review: 'Barde Radiance really the best ever in India mostly Face-lift service is a cosmetic surgical procedure to create a younger appearance in your face. The procedure can reduce the sagging. really the best ever in India mostly Face-lift',
+  },
+];
 
 function Testimonial() {
   return (
-    <>
-       <Slider className='overflow-hidden h-[850px] md:h-[750px] lg:h-[800px] pt-10 bg-[#FDF2FF] '   {...sliderSettings}>
-        {TestimonialData.map((data,index)=>(
-        <div key={index} className='flex flex-col justify-center relative items-center md:min-h-screen w-screen bg-[#FDF2FF]  '>
+    <Slider className='overflow-hidden h-[850px] md:h-[750px] lg:h-[800px] pt-10 bg-[#FDF2FF]' {...sliderSettings}>
+      {TestimonialData.map((data, index) => (
+        <div key={index} className='flex flex-col justify-center relative items-center md:min-h-screen w-screen bg-[#FDF2FF]'>
+          <img src={WhitePlant} alt="" className='absolute !hidden md:!block -bottom-20 xl:bottom-16 2xl:-bottom-8 left-0 mix-blend-plus-lighter max-w-[343px] z-0' />
+          <img src={Leaf} alt="" className='absolute md:right-20 top-1/2 right-0 md:top-36 lg:top-28 xl:top-20 max-w-[343px] z-0' />
+          <img src={Whiteobject} alt="" className='absolute bottom-5 right-[10vw] lg:right-[20vw] xl:right-[30vw] md:bottom-20 lg:bottom-7 xl:bottom-24 max-w-[200px] z-0' />
 
-              <img src={WhitePlant} alt="" className='absolute !hidden md:!block -bottom-20 xl:bottom-16 2xl:-bottom-8 left-0 mix-blend-plus-lighter max-w-[343px] z-0 ' />
-              <img src={Leaf} alt="" className='absolute md:right-20 top-1/2 right-0 md:top-36 lg:top-28 xl:top-20  max-w-[343px] z-0 ' />
-              <img src={Whiteobject} alt="" className='absolute bottom-5 right-[10vw] lg:right-[20vw] xl:right-[30vw]  md:bottom-20 lg:bottom-7 xl:bottom-24  max-w-[200px] z-0 ' />
+          <div className='flex flex-col justify-center mb-10 items-center gap-5'>
+            <h2 className="font-bold font-ElMessiri text-[#554075] leading-tight text-center px-1 z-10" style={{ fontSize: 'clamp(36px,4vw,55px)' }}>
+              What Our Patients Says
+            </h2>
+            <p className='text-sm sm:text-base w-[97%] md:w-[80%] lg:w-[60%] xl:w-[30%] text-center text-[#554075] z-10'>
+              Here are some of the reviews that our patients gave us according to our treatments on Google
+            </p>
+          </div>
 
-            <div className='flex flex-col justify-center mb-10 items-center gap-5  '>
-                <h2 className=" font-bold font-ElMessiri text-[#554075] leading-tight text-center px-1 z-10 " style={{fontSize:'clamp(36px,4vw,55px)'}}>
-                What Our Patients Says
-                </h2>
-                <p className='text-sm sm:text-base w-[97%] md:w-[80%] lg:w-[60%] xl:w-[30%] text-center text-[#554075] z-10'>Here are some of the reviews that our patients gave us according to our treatments on Google</p>
-            </div>
-
-            <div className='flex flex-col gap-10 justify-center mb-10 items-center'>
-            <div className=' w-[180px] h-[180px] md:w-[200px] md:h-[200px] lg:w-[260px] lg:h-[260px] relative aspect-square rounded-full '>
-                {/* <img src={testimonial} alt="" className='object-cover ' /> */}
-               
-               <div className='overflow-hidden rounded-full w-full h-full bg-black'><video 
-                            // id={`video-${index}`}
-                            // ref={videoRef}
-                            src={data.video} 
-                            className="w-full h-full   " 
-                            autoPlay
-                            muted
-                            loop
-                            playsInline
-                            controls
-                            // onEnded={() => setPlayingVideo(null)}
-                          />
-                          </div> 
-                <div className=' p-2 md:p-3 lg:p-4 rounded-full bg-white absolute right-0 bottom-0 '>
-                  <img src={InvertedComa} alt="" className='w-8' />
-                </div>
-            </div>
-
-            <div className='flex flex-col'>
-                <h3 className='text-4xl text-[#CDAE62] font-ElMessiri z-10'>{data.Name}</h3>
-                <p className='text-[#554075] text-center z-10'>{data.Position}</p>
-            </div>
-
-            <p className='text-[#554075] text-center w-[90vw] lg:w-[76vw] xl:w-[54vw] 2xl:w-[51%] z-10 '>‘‘{data.Review}’’</p>
-            </div>
+          <TestimonialSlide data={data} />
         </div>
-      )) }
-        
-        </Slider>
-    </>
-  )
+      ))}
+    </Slider>
+  );
 }
 
-export default Testimonial
+export default Testimonial;
