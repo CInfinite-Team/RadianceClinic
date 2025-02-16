854fe8f1bda955fa925a1843db41a7a94a1c1fb7
JiteshVarade <jiteshvarade1@gmail.com>
2025-02-16 12:47:06 +0530

    Helmet provider added for few pages only, SEO needs to be better

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -5,6 +5,8 @@ import ContactSection from '../components/ContactUs/ContactSection'
 import ContactPlate from '../components/ContactUs/ContactPlate'
 import Map from '../components/ContactUs/Map'
 import Footer from '../components/footer/Footer'
+import { Helmet } from 'react-helmet-async'
+
 function ContactUs() {
 
   useEffect(() => {
@@ -13,6 +15,17 @@ function ContactUs() {
 
   return (
     <>
+      <Helmet>
+        <title>Contact Radiance Clinic: Expert Skin & Hair Transplant, Nagpur</title>
+        <meta
+        name="description"
+        content="Get in touch with Radiance Clinic for appointments and
+        inquiries. Reach out to our Nagpur clinic for expert skin and hair
+        treatments."
+        />
+        <link rel="canonical"
+        href="https://radianceclinic.co.in/contact-us" />
+      </Helmet>
       <Navbar/>
       <HeroSectionAll Title={'Contact Us'} Path={'Home'} SubPath={'Contact Us'} />
       <ContactSection/>
