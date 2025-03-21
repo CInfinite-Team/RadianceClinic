492fd8e5dde9a6fc106b6e9d3e518bf0c992d010
gajju44 <gajjunaphade@gmail.com>
2025-03-21 20:58:47 +0530

    removed parameter machanism for quick links

--- client/src/components/QuickLinks/QuickLinkDescription.jsx
+++ client/src/components/QuickLinks/QuickLinkDescription.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { QuickLinkData } from './QuickLinkData'
 import HeroSectionAll from '../SharedComponents/HeroSectionAll';
 import AppointmentForm from '../Appointment/AppointmentForm';
 import Navbar from '../navbar/Navbar';
 import Footer from '../footer/Footer';
-
+import { useLocation } from 'react-router-dom';
 function QuickLinkDescription() {
-    const urlParams = new URLSearchParams(location.search);
-    // const treatmentData = JSON.parse(urlParams.get('Data'));
-    const Description = urlParams.get('Description');
-    const Heading = urlParams.get('Title');
+
+  const [Data,setData]=useState([null])
+  const location = useLocation();
+  useEffect(() => {
+    const currentPath = location.pathname;
+
+    Object.keys(QuickLinkData).forEach((city) => {
+      QuickLinkData[city].forEach((linkData) => {
+        if (currentPath.includes(linkData.href)) {
+          setData(linkData);
+        }
+      });
+    });
+  }, [location]);
   return (
     <>
     <Navbar/>
-    <HeroSectionAll Title={'Appointment'} Path={'Home'} SubPath={'Appointment'} />
+    <HeroSectionAll Title={'Book Your Appointment'} Path={'Home'} SubPath={'Book Your Appointment'} />
     <AppointmentForm/>
     <div className='bg-[#fef7ff] py-24 px-4 md:px-4 xl:px-20'>
     <h1 className='font-ElMessiri text-[#554075] text-center font-bold leading-tight animate-fadeIn' style={{ fontSize: 'clamp(36px,6vw,60px)' }}>
-            {Heading}
+            {Data.text}
             </h1>
-       <p className='font-semibold text-center text-[#392c4e]'>{Description}</p>
+       <p className='font-semibold text-center text-[#392c4e]'>{Data.description}</p>
     </div>
     <Footer/>
     </>
